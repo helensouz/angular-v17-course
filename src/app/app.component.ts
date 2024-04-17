0f5952d11e49c88a244c7024f8f5e1d263c0c056
helensouzaoliveiraibm <Helen.Souza.Oliveira@ibm.com>
2024-04-17 00:48:07 -0300

    observables, promisses e funcoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+     HttpClientModule,
+     FormsModule,
+      FontAwesomeModule],
+  providers: [HttpClient ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
+
 export class AppComponent {
   title = 'angular-v17-course';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { WeatherComponent } from './modules/weather/page/weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '', redirectTo: 'weather', pathMatch: 'full'
+  },
+
+  {
+    path: 'weather',
+    component: WeatherComponent
+  }
+];
